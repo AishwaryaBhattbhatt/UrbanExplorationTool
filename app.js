0b97b75f50d45df1cf90e1d9d5228eb0425fbb48
AishwaryaBhattbhatt <147745283+AishwaryaBhattbhatt@users.noreply.github.com>
2024-10-03 14:16:25 -0400

    1

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-let map;
-const HEXAGON_DIAMETER_METERS = 100;
-let hexGrid;
-let revealedHexagons = new Set();
+var map;
+var HEXAGON_DIAMETER_METERS = 100;
+var hexGrid;
+var revealedHexagons = new Set();
 
 function initMap() {
     updateStatus("Initializing map...");
@@ -29,7 +29,7 @@ function initMap() {
         hexGrid = new google.maps.OverlayView();
         hexGrid.onAdd = function() {
             updateStatus("Hex grid onAdd called.");
-            const svg = d3.select(this.getPanes().overlayMouseTarget).append("svg")
+            var svg = d3.select(this.getPanes().overlayMouseTarget).append("svg")
                 .attr("id", "hexSvg")
                 .style("position", "absolute");
             this.svg = svg;
@@ -42,49 +42,49 @@ function initMap() {
                 return;
             }
             this.svg.selectAll("*").remove();
-            const overlayProjection = this.getProjection();
-            const bounds = map.getBounds();
-            const ne = bounds.getNorthEast();
-            const sw = bounds.getSouthWest();
-            const topLeft = overlayProjection.fromLatLngToDivPixel(new google.maps.LatLng(ne.lat(), sw.lng()));
-            const bottomRight = overlayProjection.fromLatLngToDivPixel(new google.maps.LatLng(sw.lat(), ne.lng()));
+            var overlayProjection = this.getProjection();
+            var bounds = map.getBounds();
+            var ne = bounds.getNorthEast();
+            var sw = bounds.getSouthWest();
+            var topLeft = overlayProjection.fromLatLngToDivPixel(new google.maps.LatLng(ne.lat(), sw.lng()));
+            var bottomRight = overlayProjection.fromLatLngToDivPixel(new google.maps.LatLng(sw.lat(), ne.lng()));
 
-            const width = bottomRight.x - topLeft.x;
-            const height = bottomRight.y - topLeft.y;
+            var width = bottomRight.x - topLeft.x;
+            var height = bottomRight.y - topLeft.y;
 
             this.svg.style("left", topLeft.x + "px")
                 .style("top", topLeft.y + "px")
                 .style("width", width + "px")
                 .style("height", height + "px");
 
-            const center = map.getCenter();
-            const pixelsPerMeter = this.getPixelsPerMeter(center.lat());
-            const hexRadius = (HEXAGON_DIAMETER_METERS / 2) * pixelsPerMeter;
+            var center = map.getCenter();
+            var pixelsPerMeter = this.getPixelsPerMeter(center.lat());
+            var hexRadius = (HEXAGON_DIAMETER_METERS / 2) * pixelsPerMeter;
 
-            const hexbin = d3.hexbin()
+            var hexbin = d3.hexbin()
                 .radius(hexRadius)
                 .extent([[0, 0], [width, height]]);
 
-            const points = [];
-            for (let x = 0; x < width; x += hexRadius * Math.sqrt(3)) {
-                for (let y = 0; y < height; y += hexRadius * 3) {
+            var points = [];
+            for (var x = 0; x < width; x += hexRadius * Math.sqrt(3)) {
+                for (var y = 0; y < height; y += hexRadius * 3) {
                     points.push([x, y]);
                     points.push([x + hexRadius * Math.sqrt(3) / 2, y + hexRadius * 1.5]);
                 }
             }
 
-            const hexagons = this.svg.selectAll("path")
+            var hexagons = this.svg.selectAll("path")
                 .data(hexbin(points))
                 .enter().append("path")
-                .attr("d", d => "M" + d.x + "," + d.y + hexbin.hexagon())
+                .attr("d", function(d) { return "M" + d.x + "," + d.y + hexbin.hexagon(); })
                 .attr("class", "hexagon")
-                .attr("id", (d, i) => `hex-${i}`)
+                .attr("id", function(d, i) { return "hex-" + i; })
                 .on("mousedown", handleHexagonInteraction)
                 .on("touchstart", handleHexagonInteraction);
 
-            revealedHexagons.forEach(id => {
-                this.svg.select(`#${id}`).classed("revealed", true);
-            });
+            revealedHexagons.forEach(function(id) {
+                this.svg.select("#" + id).classed("revealed", true);
+            }.bind(this));
 
             updateStatus("Hex grid drawn.");
         };
@@ -95,12 +95,12 @@ function initMap() {
 
         hexGrid.setMap(map);
 
-        google.maps.event.addListenerOnce(map, 'idle', () => {
+        google.maps.event.addListenerOnce(map, 'idle', function() {
             updateStatus("Map idle. Requesting geolocation...");
             requestGeolocation();
         });
 
-        google.maps.event.addListener(map, 'bounds_changed', () => {
+        google.maps.event.addListener(map, 'bounds_changed', function() {
             updateStatus("Bounds changed. Redrawing hex grid...");
             hexGrid.draw();
         });
@@ -114,14 +114,15 @@ function initMap() {
 function requestGeolocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
-            (position) => {
+            function(position) {
                 updateStatus("Geolocation received. Centering map...");
-                const { latitude, longitude } = position.coords;
+                var latitude = position.coords.latitude;
+                var longitude = position.coords.longitude;
                 map.setCenter({ lat: latitude, lng: longitude });
                 map.setZoom(15);
                 hexGrid.draw();
             },
-            (error) => {
+            function(error) {
                 updateStatus("Geolocation error: " + error.message);
                 hexGrid.draw();
             }
@@ -135,7 +136,7 @@ function requestGeolocation() {
 function handleHexagonInteraction(event, d) {
     event.preventDefault();
     event.stopPropagation();
-    const hexId = d3.select(this).attr("id");
+    var hexId = d3.select(this).attr("id");
     d3.select(this).classed("revealed", true);
     revealedHexagons.add(hexId);
     updateStatus("Hexagon revealed: " + hexId);
